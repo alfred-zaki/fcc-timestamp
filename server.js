a6dbdab678bedc5f04af51f67bb17110f5880910
Alfred Zaki <alfred.erian@gmail.com>
2017-02-16 04:42:34 +0000

    It's done

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 var express = require("express");
+var dateFormat = require('dateformat');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:userTime', function (req, res) {
+  var userTimeInputStr = req.params.userTime;
+  var userTimeNaturel;
+  var userTimeUnix;
+  var response;
+  if (Number.isNaN(parseInt(userTimeInputStr))){
+    userTimeUnix = Math.round((new Date(userTimeInputStr)).getTime() / 1000);
+    if (Number.isNaN(userTimeUnix)){
+      //not both return null
+      response = {unix : null, natural: null};
+      res.send(response);
+      return;
+    }else{
+      //natural
+      userTimeNaturel = userTimeInputStr;
+      response = {unix : userTimeUnix, natural: userTimeNaturel};
+      res.send(response);
+      return;
+    }
+  }else{
+    //Unix
+    userTimeNaturel = dateFormat(new Date(parseInt(userTimeInputStr) * 1000), 'longDate');
+    userTimeUnix = parseInt(userTimeInputStr);
+    response = {unix : userTimeUnix, natural: userTimeNaturel};
+    res.send(response);
+    return;
+  }
+});
+
+app.get('/', function(req, res){
+  res.send('Alfred Zaki FreeCodeCamp TimeStamp Project');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Timestamp app listening on port 8080!');
 });
